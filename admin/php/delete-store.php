542a22ccf239b59f3a2185efa53af9d89919c826
XtremeTheory <dillon.millsap@live.com>
2018-05-13 15:31:25 -0400

    Delete
    Remove files not needed

---
+++ admin/php/delete-store.php
@@ -0,0 +1,23 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+require 'db.php';
+require 'functions.php';
+$sid = test_input($_POST['sid']);
+$uid = 1;
+
+	$query = "DELETE FROM store_data WHERE id = '$sid'";
+	$result = $test_db->query($query);
+	if($result){
+    echo "complete";
+    mysqli_close($test_db);
+    exit();
+	} else {
+    $sqlError = mysqli_error($test_db);
+    logError("1","delete-store.php",$uid,$sqlError);
+    echo "servfailure";
+    mysqli_close($test_db);
+    exit();
+	}
+?>
