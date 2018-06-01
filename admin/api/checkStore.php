a27bafde0ee0a9b1b7411c01128aa5fc1a637b9f
XtremeTheory <dillon.millsap@live.com>
2018-06-01 10:07:08 -0400

    6/1/18 Morning

--- admin/api/checkStore.php
+++ admin/api/checkStore.php
@@ -1,5 +1,5 @@
 <?php
-$sname = test_input($_POST['store']);
+$storezip = test_input($_POST['storezip']);
 $token = $_POST['token'];
 global $timestamp;
 $apiresult = array();
@@ -30,12 +30,12 @@ if($rowcount != 1) {
   exit();
 }
 
-$query = "SELECT * FROM store_data WHERE sname = '$sname'";
+$query = "SELECT * FROM city_list WHERE zipcode = '$storezip'";
 $result = $test_db->query($query);
 
 if(!$result) {
   $sqlError = mysqli_error($test_db);
-  logError("1","getStore.php","0",$sqlError);
+  logError("1","checkStore.php","0",$sqlError);
   $errors["errorStatus"] = "true";
   $errors["errorMessage"] = "Issues with the server. It has been reported.";
   $apiresult["error"] = $errors;
@@ -48,21 +48,21 @@ $rowcount = mysqli_num_rows($result);
 
 if($rowcount != 1) {
   $errors["errorStatus"] = "true";
-  $errors["errorMessage"] = "Store is not in the system.";
+  $errors["errorMessage"] = "Incorrect zip code.";
   $apiresult["error"] = $errors;
   echo json_encode($apiresult);
   mysqli_close($test_db);
   exit();
 }
 
-$prodinfo = $result->fetch_assoc();
-$bid = $prodinfo['brand'];
-$query1 = "SELECT * FROM brands WHERE id = '$bid'";
+$zipinfo = $result->fetch_assoc();
+$zid = $zipinfo['id'];
+$query1 = "SELECT * FROM store_data WHERE cid = '$zid'";
 $result1 = $test_db->query($query1);
 
 if(!$result1) {
   $sqlError = mysqli_error($test_db);
-  logError("1","getProduct.php","0",$sqlError);
+  logError("1","checkStore.php","0",$sqlError);
   $errors["errorStatus"] = "true";
   $errors["errorMessage"] = "Issues with the server. It has been reported.";
   $apiresult["error"] = $errors;
@@ -71,17 +71,13 @@ if(!$result1) {
   exit();
 }
 
-$brandinfo = $result1->fetch_assoc();
+$storeinfo = $result1->fetch_assoc();
 
-$prod["pname"] = $prodinfo['pname'];
-$prod["brand"] = $brandinfo['bname'];
-$prod["dept"] = $prodinfo['dept'];
-$prod["msize"] = $prodinfo['msize'];
-$prod["mtype"] = $prodinfo['measure'];
-$prod["exist"] = "true";
+$store["sname"] = $storeinfo['sname'];
+$store["address"] = $storeinfo['address'];
 $errors["errorStatus"] = "false";
 $apiresult["error"] = $errors;
-$apiresult["prod"] = $prod;
+$apiresult["store"] = $store;
 echo json_encode($apiresult);
 mysqli_close($test_db);
 exit();
